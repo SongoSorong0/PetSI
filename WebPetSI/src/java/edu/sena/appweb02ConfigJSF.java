97a1057128df3516f1eb9874db56a838253357b4
migue <migue@192.168.0.9>
2020-12-09 20:47:16 -0500

    Se configuró el xhtml

---
+++ WebPetSI/src/java/edu/sena/appweb02ConfigJSF.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.sena;
+
+import javax.faces.annotation.FacesConfig;
+
+import static  javax.faces.annotation.FacesConfig.Version.JSF_2_3;
+/**
+ *
+ * @author migue
+ */
+@FacesConfig(version = JSF_2_3)
+public class appweb02ConfigJSF {
+    
+}
